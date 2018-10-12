4505d495ce21b803c46b636edef8006c9a1e4cc8
SamuelVera <samuelvera1999@gmail.com>
2018-10-12 14:12:04 -0400

    Navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { DetailsComponent } from './details/details.component';
+import { DetailsComponent } from './home/details/details.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -10,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [RouterModule.forRoot(routes) ],
   exports: [RouterModule],
 })
 
