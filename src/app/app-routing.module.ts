b2c87b6a56a037e7a2222f91cbe2005c791b4d7f
SamuelVera <samuelvera1999@gmail.com>
2018-10-12 18:10:27 -0400

    Agregado url "único" para buscar en el .json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DetailsComponent } from './home/details/details.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'details', component: DetailsComponent },
+  { path: 'details/:_id', component: DetailsComponent },
 ];
 
 @NgModule({
